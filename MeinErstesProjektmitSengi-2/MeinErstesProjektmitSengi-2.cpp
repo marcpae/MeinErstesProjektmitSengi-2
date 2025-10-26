b2c116f6b46acfcd2a94c7440c2b927aca635735
marcpae <53708088+marcpae@users.noreply.github.com>
2025-10-26 13:38:39 +0100

    neuer branch mit loop

--- MeinErstesProjektmitSengi-2/MeinErstesProjektmitSengi-2.cpp
+++ MeinErstesProjektmitSengi-2/MeinErstesProjektmitSengi-2.cpp
@@ -1,17 +1,15 @@
-// Kommentare mit double slash 
-//Geschichte: C++ ist die Erweiterung von C, C kann bspw. keine Klassen erstellen
-
-//header dateien: .h gekennzeichnet, zB iostream, werden über #include <namederheaderfile> eingebunden
-//header files, die bereits existieren werden über die <> aufgerufen, eigene über ""
-
 #include <iostream>
-#include <string> //"visual studios" header file
-#include "test.h" //eigene header file
+#include <string> 
+#include <unordered_map> 
+#include <vector>
 
 typedef unsigned char byte; //ich kann mir auch eigene "Datentypen" erstellen
 
+
+
+
 //weil unsere construction function nicht funktioniert (wegen der int pointer geschichte in Zeile 21) machen wir noch
-//einen wrapper zB Kool Savas
+//einen wrapper 
 void* address(int* pointer, int offset) //void* ist der DATENTYP für Pointer
 {
     return (void*)((long long)pointer + (long long)offset);
@@ -19,7 +17,7 @@ void* address(int* pointer, int offset) //void* ist der DATENTYP für Pointer
 
 
 //in construction legen wir 4 bytes in einen integer ab
-void construction(int* dst, byte*b1, byte* b2, byte* b3, byte* b4)
+void construction(int* dst, byte* b1, byte* b2, byte* b3, byte* b4)
 {
     //kopiere b1 in dst
     std::memcpy(address(dst, 0), b1, 1); //wir nehmen dst und schreiben d1 in das erste BYTE
@@ -29,6 +27,18 @@ void construction(int* dst, byte*b1, byte* b2, byte* b3, byte* b4)
     return; //optional (aber natürlich OHNE rückgabeparameter weil void
 }
 
+
+//dummer loop um construction umzusetzen
+void constructionloop(int* dst, b[])
+{
+    for (int i = 0; i < 4; i++)
+    {
+        std::memcpy(address(dst, i), b[i], 1);
+    }
+}
+
+
+
 int main() //void: meine applikation gibt immer 0 zurück, int: meine applikation kann einen rückgabewert liefern - führt nur irgendwas aus
 {
     int speicher = 5; //int: 4byte
@@ -36,20 +46,31 @@ int main() //void: meine applikation gibt immer 0 zurück, int: meine applikatio
     //zahlenbasierten zahlentypen
 
     long long schlong1 = 540; //nur "long" kann auch int sein, "long long" ist immer 64bit also 8 byte
-        short short1; //2byte
+    short short1; //2byte
     char char1 = 90; //1 byte, man kann nur zahlen zuweisen und ist genau 1 byte groß 
 
-    byte b1 = 1;
-    byte b2 = 1;
-    byte b3 = 1;
-    byte b4 = 1;
- 
-    construction(&speicher, &b1, &b2, &b3, &b4);
+    //Array mit 4 Variablen des Datentyps "byte" (Index 0,1,2,3)
+
+    int SizeArrayByte = 4;
+    byte b[SizeArrayByte];
+
+    //Zuweisen der Werte:
+    for (int i = 0; i < SizeArrayByte; i++)
+    {
+        b[i] = 1;
+    }
 
 
-    // Orte von Variablen (wo sie im speicher zu finden sind) werden mit "&" aufgerufen
-    std::cout << "Hello Dirq!\n";
+
+
+    construction(&speicher, &b[0], &b[1], &b[2], &b[4]);
+    constructionloop(&speicher, &b[0], &b[1], &b[2], &b[4])
+
+
+        // Orte von Variablen (wo sie im speicher zu finden sind) werden mit "&" aufgerufen
+        std::cout << "Hello Dirq!\n";
     std::cout << char1 << std::endl;
     std::cout << speicher << std::endl;
 }
+
 
