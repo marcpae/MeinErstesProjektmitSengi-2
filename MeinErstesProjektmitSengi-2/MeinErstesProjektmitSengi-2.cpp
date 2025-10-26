61c3c76df8a7a2d6427a24e254052a0698acf1ec
marcpae <53708088+marcpae@users.noreply.github.com>
2025-10-26 15:01:09 +0100

    Loop korrigiert

--- MeinErstesProjektmitSengi-2/MeinErstesProjektmitSengi-2.cpp
+++ MeinErstesProjektmitSengi-2/MeinErstesProjektmitSengi-2.cpp
@@ -6,15 +6,12 @@
 typedef unsigned char byte; //ich kann mir auch eigene "Datentypen" erstellen
 
 
-
-
 //weil unsere construction function nicht funktioniert (wegen der int pointer geschichte in Zeile 21) machen wir noch
 //einen wrapper 
 void* address(int* pointer, int offset) //void* ist der DATENTYP für Pointer
 {
     return (void*)((long long)pointer + (long long)offset);
 }
-
 
 //in construction legen wir 4 bytes in einen integer ab
 void construction(int* dst, byte* b1, byte* b2, byte* b3, byte* b4)
@@ -29,14 +26,15 @@ void construction(int* dst, byte* b1, byte* b2, byte* b3, byte* b4)
 
 
 //dummer loop um construction umzusetzen
-void constructionloop(int* dst, b[])
+void constructionloop(int* dst, byte bytes_array[], const int array_size)
 {
+    if (array_size < 4) return;
+
     for (int i = 0; i < 4; i++)
     {
-        std::memcpy(address(dst, i), b[i], 1);
+        std::memcpy(address(dst, i), &bytes_array[i], 1); // & Zeichen vor bytes_array, damit er die ADRESSE nimmt (die pointer geschichte..)
     }
 }
-
 
 
 int main() //void: meine applikation gibt immer 0 zurück, int: meine applikation kann einen rückgabewert liefern - führt nur irgendwas aus
@@ -50,9 +48,12 @@ int main() //void: meine applikation gibt immer 0 zurück, int: meine applikatio
     char char1 = 90; //1 byte, man kann nur zahlen zuweisen und ist genau 1 byte groß 
 
     //Array mit 4 Variablen des Datentyps "byte" (Index 0,1,2,3)
+    //ES MUSS EIN CONST INT ANGELEGT WERDEN, KEINE ÄNDERBARE VARIABLE FÜR DIE ARRAYGROESSE!
 
-    int SizeArrayByte = 4;
-    byte b[SizeArrayByte];
+    const int SizeArrayByte = 4;
+    const int  AmountArrayElements = 4;
+    // byte b[SizeArrayByte];
+    byte* b = (byte*) malloc (AmountArrayElements * sizeof(byte)); //malloc allociert memory, jetzt reservieren wir 4 "bytes", 4 stellen als byte-pointer (das ist also ein pointer, der bytes pointet)
 
     //Zuweisen der Werte:
     for (int i = 0; i < SizeArrayByte; i++)
@@ -61,10 +62,9 @@ int main() //void: meine applikation gibt immer 0 zurück, int: meine applikatio
     }
 
 
+    constructionloop(&speicher, b, SizeArrayByte);
 
-
-    construction(&speicher, &b[0], &b[1], &b[2], &b[4]);
-    constructionloop(&speicher, &b[0], &b[1], &b[2], &b[4])
+    free(b);
 
 
         // Orte von Variablen (wo sie im speicher zu finden sind) werden mit "&" aufgerufen
